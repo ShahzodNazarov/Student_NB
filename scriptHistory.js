65bef5981083f33321229e046962d368973f4e5b
ShakhzodNazarov <shaxa4536@gmail.com>
2024-04-27 13:45:12 +0500

    third

--- scriptHistory.js
+++ scriptHistory.js
@@ -17,8 +17,7 @@ function drawSavedArray() {
     sum += `
             <tr class=${savedArray[i].idNum == 1 && "table-success"}>
             <td>${savedArray[i].idNum}</td>
-            <td>${savedArray[i].firstname}</td>
-            <td>${savedArray[i].lastname}</td>
+            <td>${savedArray[i].firstname} ${savedArray[i].lastname}</td>
             <td class=${savedArray[i].isChecked == false ? 'text-danger':'text-success'}>
             ${savedArray[i].isChecked ? "bor" : "yo'q"}</td>
             <td>${savedArray[i].date}</td>
